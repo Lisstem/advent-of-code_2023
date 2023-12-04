23b025ba1dc5ffa6c33bd68f763137bdc2348701
lisstem <mail@lisstem.net>
2023-12-04 14:03:02 +0100

    Improve solution Day 3 part 1

--- 03/solution_part_one.rb
+++ 03/solution_part_one.rb
@@ -7,26 +7,19 @@ def string_symbol?(line, first, last)
     line&.match(/[^\.]/)
 end
 
-def symbol?(schematic, line, first, last)
-    char_symbol?(schematic[line][first - 1]) ||
-    char_symbol?(schematic[line][last + 1]) ||
-    string_symbol?(schematic[line-1], first - 1, last + 1) ||
-    string_symbol?(schematic[line+1], first - 1, last + 1)
+def symbol?(first, last, prior, line, after)
+    char_symbol?(line[first - 1]) ||
+    char_symbol?(line[last]) ||
+    string_symbol?(prior, first - 1, last) ||
+    string_symbol?(after, first - 1, last)
+end
+
+def find_numbers(prior, line, after)
+    line.to_enum(:scan, /\d+/).map { Regexp.last_match}.each_with_object([prior, line, after])
 end
 
 file = File.open "input.txt"
-schematic = file.readlines.map { |line| "." + line.strip + "." }.prepend("").append("")
-line_matches = schematic.map { |line| line.to_enum(:scan, /\d+/).map { Regexp.last_match } }
-numbers = line_matches.each_with_index.flat_map do |matches, i|
-    matches.map do |match|
-        first, last = match.offset(0)
-        if symbol? schematic, i, first, last -1
-            match.match(0).to_i
-        else
-            nil
-        end
-    end
-end.compact
+schematic = [""].chain(file.readlines.map { |line| "." + line.strip + "." }, [""])
+numbers = schematic.each_cons(3).map { |lines| find_numbers(*lines) }.inject(:chain).select { |match, lines| symbol?(*match.offset(0), *lines) }.map { |match, lines| match.match(0).to_i }
 
-puts numbers.inspect
 puts numbers.sum
